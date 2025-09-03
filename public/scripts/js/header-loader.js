daa1a60b1806784a4846a11b0d5c499bffe4c00e
fishernine <152625766+fishernine@users.noreply.github.com>
2025-09-03 17:53:33 +0800

    Fix navigation links for research categories
    - Updated header.html navigation links to use research.html with tag parameters
    - Fixed header-loader.js navigation links to use correct relative paths
    - Resolved 404 errors for Antenna, Filter, Analog IC, and RFIC navigation items
    - All research category links now properly filter the main research page

--- public/scripts/js/header-loader.js
+++ public/scripts/js/header-loader.js
@@ -43,10 +43,10 @@ class HeaderLoader {
                                                     <li class="menu-item-has-children">
                             <a href="javascript:void(0);">Research</a>
                             <ul>
-                                <li><a href="../pages/research.html?tag=antenna">Antenna</a></li>
-                                <li><a href="../pages/research.html?tag=filter">Filter</a></li>
-                                <li><a href="../pages/research.html?tag=analog ic">Analog IC</a></li>
-                                <li><a href="../pages/research.html?tag=rfic">RFIC</a></li>
+                                <li><a href="research.html?tag=antenna">Antenna</a></li>
+                                <li><a href="research.html?tag=filter">Filter</a></li>
+                                <li><a href="research.html?tag=analog ic">Analog IC</a></li>
+                                <li><a href="research.html?tag=rfic">RFIC</a></li>
                             </ul>
                         </li>
                             <li><a href="blog-grid-lsb.html">Event</a></li>
